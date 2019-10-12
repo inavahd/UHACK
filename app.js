4c4a2f23fbc29bbace4657c9130b4caf4a44e24c
Dhavani Reddy <42347787+inavahd@users.noreply.github.com>
2019-10-12 16:32:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var app = express()
+var mongoose = require("mongoose")
+var passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    User        = require("./models/user");
+
+mongoose.connect("mongodb://localhost:27017/uhack", {useNewUrlParser: true})
+
+app.set("view-engine","ejs")
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(require("express-session")({
+    secret: "COE-2 is shit!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   next();
+})
+
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get('/',function(req,res){
+    res.render("landing.ejs");
+})
+
+
+
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+app.listen(3000, function(){
+    console.log("The app has started!!");
+})
